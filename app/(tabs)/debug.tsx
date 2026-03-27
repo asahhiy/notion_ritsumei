bc548d69f1d63a48f0fa995d7453260fb871e0e1
asahhiy <asahhiy@gmail.com>
2026-03-27 13:57:27 +0900

    done: 更新スクリプトOK, pageIDだけ指定忘れなければ大丈夫

--- app/(tabs)/debug.tsx
+++ app/(tabs)/debug.tsx
@@ -1,6 +1,7 @@
 import ClassNumberPicker from "@/components/debug/class-number-picker";
 import { migrateDatabase } from "@/utils/migrate";
 import { seedDatabase } from "@/utils/seed";
+import { updateLessonData } from "@/utils/updatelessondata";
 import { Button, ScrollView, Text, View } from "react-native";
 
 export default function DebugScreen() {
@@ -18,7 +19,22 @@ export default function DebugScreen() {
         title="Execute Seeding"
         onPress={async () => await seedDatabase()}
       />
-      <View className="h-[500px]" />
+      <View className="h-[100px]" />
+      <Button
+        title="試しに更新"
+        onPress={async () => {
+          await updateLessonData({
+            pageId: "32d9f424ebfa80e89be6f3af270fcf44",
+            subjectName: "更新テストseikou",
+            semester: "2年前期",
+            when: 1,
+            day: "tuesday",
+            place: "オンライン",
+            professor: "山田太郎",
+            syllabusUrl: "https://example.com/syllabus",
+          });
+        }}
+      />
     </ScrollView>
   );
 }
